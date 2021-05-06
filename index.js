157723b27ac08d962008c0386f85fab371de018e
Astrawan Wayan <astra@protonmail.com>
2021-05-06 17:47:24 +0800

    Add OpenBSD platform

--- index.js
+++ index.js
@@ -13,6 +13,14 @@ function linux (name) {
   return path.join(process.env.HOME, '.config', name)
 }
 
+function openbsd (name) {
+  if (process.env.XDG_CONFIG_HOME) {
+    return path.join(process.env.XDG_CONFIG_HOME, name)
+  }
+
+  return path.join(process.env.HOME, '.config', name)
+}
+
 function win32 (name) {
   if (process.env.LOCALAPPDATA) {
     return path.join(process.env.LOCALAPPDATA, name)
@@ -29,6 +37,7 @@ function applicationConfigPath (name) {
   switch (os.platform()) {
     case 'darwin': return darwin(name)
     case 'linux': return linux(name)
+    case 'openbsd': return openbsd(name)
     case 'win32': return win32(name)
   }
 
